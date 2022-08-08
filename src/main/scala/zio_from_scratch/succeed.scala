a26d3a497c8c90bd396cdb8c4ca47c9dc7138e63
jaacko-torus <jaacko.torus@gmail.com>
2022-08-08 15:20:48 -0500

    Part 1

--- src/main/scala/zio_from_scratch/succeed.scala
+++ src/main/scala/zio_from_scratch/succeed.scala
@@ -9,6 +9,119 @@ object Person:
   val peter: Person = Person("Peter", 88)
 
 trait ZIOApp:
-  def run: ZIO[Any] = ???
+  def run: ZIO[Any]
   def main(args: Array[String]): Unit =
-    println("default value")
+    run.run { result =>
+      println(s"value: ${result}")
+    }
+
+    Thread.sleep(5000)
+
+object succeedNow extends ZIOApp:
+  val peterZIO: ZIO[Person] =
+    ZIO.succeedNow(Person.peter)
+  override def run: ZIO[Person] = peterZIO
+
+object succeedNowUhOh extends ZIOApp:
+  val howdyZIO =
+    ZIO.succeedNow(println("Howdy 🏇🤠"))
+  override def run: ZIO[Any] = ZIO.succeedNow(1)
+
+object succeed extends ZIOApp:
+  val howdyZIO =
+    ZIO.succeed(println("Howdy 🏇🤠"))
+  override def run: ZIO[Any] = ZIO.succeedNow(1)
+
+object succeedAgain extends ZIOApp:
+  def printLine(message: String): ZIO[Unit] =
+    ZIO.succeed(println(message))
+  override def run: ZIO[Any] = printLine("Fancy 🍴")
+
+object zip extends ZIOApp:
+  val zippedZIO: ZIO[(Int, String)] =
+    ZIO.succeed(8) `zip` ZIO.succeed("LO")
+  override def run: ZIO[Any] = zippedZIO
+
+object map extends ZIOApp:
+  val zippedZIO: ZIO[(Int, String)] =
+    ZIO.succeed(8) `zip` ZIO.succeed("LO")
+  val personZIO: ZIO[Person] = zippedZIO.map((i, s) => Person(s, i))
+  val mappedZIO: ZIO[String] = zippedZIO.map((i, s) => s * i)
+  override def run: ZIO[Any] = mappedZIO
+
+object mapUhOh extends ZIOApp:
+  val zippedZIO: ZIO[(Int, String)] =
+    ZIO.succeed(8) `zip` ZIO.succeed("LO")
+
+  def printLine(message: String): ZIO[Unit] =
+    ZIO.succeed(println(message))
+
+  val mappedZIO = zippedZIO.map { tuple =>
+    printLine(s"tuple: $tuple")
+  }
+
+  override def run: ZIO[Any] = mappedZIO
+
+object flatMap extends ZIOApp:
+  val zippedZIO: ZIO[(Int, String)] =
+    ZIO.succeed(8) `zip` ZIO.succeed("LO")
+
+  def printLine(message: String): ZIO[Unit] =
+    ZIO.succeed(println(message))
+
+  val flatMappedZIO = zippedZIO.flatMap { tuple =>
+    printLine(s"tuple: $tuple")
+  }
+
+  override def run: ZIO[Any] = flatMappedZIO
+
+object forComprehension extends ZIOApp:
+  val zippedZIO: ZIO[(Int, String)] =
+    ZIO.succeed(8) `zip` ZIO.succeed("LO")
+
+  def printLine(message: String): ZIO[Unit] =
+    ZIO.succeed(println(message))
+
+  val flatMappedZIO =
+    for
+      tuple <- zippedZIO
+      _     <- printLine(s"tuple: $tuple")
+    yield ()
+
+  override def run: ZIO[Any] = flatMappedZIO
+
+object async extends ZIOApp:
+  val asyncZIO: ZIO[Int] = ZIO.async[Int] { complete =>
+    println("Async start")
+    Thread.sleep(1000)
+    complete(10)
+  }
+  override def run = asyncZIO
+
+object fork extends ZIOApp:
+  def printLine(message: String): ZIO[Unit] =
+    ZIO.succeed(println(message))
+  val asyncZIO: ZIO[Int] = ZIO.async[Int] { complete =>
+    println("Async start")
+    Thread.sleep(1000)
+    complete(scala.util.Random.nextInt(999))
+  }
+  val forkedZIO =
+    for
+      fiber1 <- asyncZIO.fork
+      fiber2 <- asyncZIO.fork
+      _      <- printLine("Done forking")
+      int1   <- fiber1.join
+      int2   <- fiber2.join
+    yield s"The ints are $int1 & $int2"
+  override def run = forkedZIO
+
+object zipPar extends ZIOApp:
+  def printLine(message: String): ZIO[Unit] =
+    ZIO.succeed(println(message))
+  val asyncZIO: ZIO[Int] = ZIO.async[Int] { complete =>
+    println("Async start")
+    Thread.sleep(1000)
+    complete(scala.util.Random.nextInt(999))
+  }
+  override def run: ZIO[Any] = asyncZIO `zipPar` asyncZIO
--- src/main/scala/zio_from_scratch/ziofs/ZIO.scala
+++ src/main/scala/zio_from_scratch/ziofs/ZIO.scala
@@ -1,4 +1,81 @@
 package jaackotorus
 package zio_from_scratch.ziofs
 
-trait ZIO[+A]
+import scala.concurrent.ExecutionContext
+
+trait Fiber[+A]:
+  def start(): Unit
+  def join: ZIO[A]
+
+class FiberImpl[A](zio: ZIO[A]) extends Fiber[A]:
+  var maybeResult: Option[A]    = None
+  var callbacks: List[A => Any] = List.empty
+  override def start(): Unit =
+    ExecutionContext.global.execute { () =>
+      zio.run { a =>
+        maybeResult = Some(a)
+        callbacks.foreach { callback =>
+          callback(a)
+        }
+      }
+    }
+  override def join: ZIO[A] = maybeResult match
+    case Some(a) => ZIO.succeedNow(a)
+    case None =>
+      ZIO.async { complete =>
+        callbacks = complete :: callbacks
+      }
+
+sealed trait ZIO[+A] { self =>
+  def run(callback: A => Unit): Unit
+
+  def flatMap[B](f: A => ZIO[B]): ZIO[B] =
+    ZIO.FlatMap(self, f)
+
+  def map[B](f: A => B): ZIO[B] =
+    flatMap { a => ZIO.succeedNow(f(a)) }
+
+  def zip[B](that: ZIO[B]): ZIO[(A, B)] =
+    for
+      a <- self
+      b <- that
+    yield (a, b)
+
+  def as[B](value: => B): ZIO[B] =
+    self.map(_ => value)
+
+  def fork: ZIO[Fiber[A]] = ZIO.Fork(self)
+
+  def zipPar[B](that: ZIO[B]): ZIO[(A, B)] =
+    for
+      fiberA <- self.fork
+      fiberB <- that.fork
+      a      <- fiberA.join
+      b      <- fiberB.join
+    yield (a, b)
+}
+
+object ZIO:
+  def succeedNow[A](value: A): ZIO[A]                         = ZIO.Succeed(value)
+  def succeed[A](value: => A): ZIO[A]                         = ZIO.Effect(() => value)
+  def async[A](register: (complete: A => Any) => Any): ZIO[A] = ZIO.Async(register)
+
+  case class Succeed[A](value: A) extends ZIO[A]:
+    override def run(callback: A => Unit): Unit = callback(value)
+
+  case class Effect[A](f: () => A) extends ZIO[A]:
+    override def run(callback: A => Unit): Unit = callback(f())
+
+  case class FlatMap[A, B](az: ZIO[A], f: A => ZIO[B]) extends ZIO[B]:
+    override def run(callback: B => Unit): Unit =
+      az.run { a =>
+        f(a).run(callback)
+      }
+  case class Async[A](register: (A => Any) => Any) extends ZIO[A]:
+    override def run(callback: A => Unit): Unit =
+      register(callback)
+  case class Fork[A](zio: ZIO[A]) extends ZIO[Fiber[A]]:
+    override def run(callback: Fiber[A] => Unit): Unit =
+      val fiber = FiberImpl(zio)
+      fiber.start()
+      callback(fiber)
